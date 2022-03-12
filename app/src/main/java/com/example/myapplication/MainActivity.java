a3ec39b797e9e973b50a9a8caf334321ae03a15c
Alexandr <minisam2468@gmail.com>
2022-03-12 23:57:05 +0600

    2

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -43,18 +44,61 @@ public class MainActivity extends AppCompatActivity {
 
         textView = (TextView) findViewById(R.id.textView);
 
+        onClickButton(button1, "1");
+        onClickButton(button2, "2");
+        onClickButton(button3, "3");
+        onClickButton(button4, "4");
+        onClickButton(button5, "5");
+        onClickButton(button6, "6");
+        onClickButton(button7, "7");
+        onClickButton(button8, "8");
+        onClickButton(button9, "9");
         button0.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View view) {
-                double result;
+                String result;
                 try {
-                    result = Double.parseDouble(textView.toString());
-                }catch (Exception e){
-                    result = 0;
+                    result = (String) textView.getText();
+                } catch (Exception e) {
+                    result = "0";
+                }
+                if (result.charAt(0) =='0'){
+                    textView.setText(result);
+                }
+                else {
+                    result += 0;
+                    textView.setText(result);
                 }
 
             }
         });
+        buttonC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText("0");
+            }
+        });
+    }
 
+    public void onClickButton(Button buttonLocal, String symbol) {
+        buttonLocal.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {
+                String result;
+                try {
+                    result = (String) textView.getText();
+                    if (result.charAt(0) =='0'){
+                        textView.setText("");
+                    }
+                } catch (Exception e) {
+                    result = "0";
+                }
+
+                result += symbol;
+                textView.setText(result);
+            }
+        });
     }
 }
