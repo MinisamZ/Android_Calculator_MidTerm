9d7de93a8930800c229abe33f974399f7b4b0fd7
Alexandr <minisam2468@gmail.com>
2022-03-12 23:17:00 +0600

    1

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.myapplication;
 
-import android.widget.Button;
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private Button button1, button2, button3, button4, button5, button6, button7, button8, button9, button0;
     private Button buttonPlus, buttonMinus, buttonC, buttonChange, buttonDivision, buttonMultiplication, buttonEquals;
     private Button buttonDot, buttonPercent;
+    private TextView textView;
 
 
 
@@ -29,18 +32,29 @@ public class MainActivity extends AppCompatActivity {
         button8 = (Button) findViewById(R.id.button8);
         button9 = (Button) findViewById(R.id.button9);
         buttonC = (Button) findViewById(R.id.button_C);
+        buttonDot = (Button) findViewById(R.id.button_dot);
         buttonPlus = (Button) findViewById(R.id.button_plus);
-                buttonMinus = (Button) findViewById()
-                buttonChange,
-                buttonDivision,
-                buttonMultiplication,
-                buttonEquals,
-                buttonDot,
-                buttonPercent
+        buttonMinus = (Button) findViewById(R.id.button_minus);
+        buttonChange = (Button) findViewById(R.id.button_change);
+        buttonEquals = (Button) findViewById(R.id.button_equals);
+        buttonPercent = (Button) findViewById(R.id.button_percent);
+        buttonDivision = (Button) findViewById(R.id.button_division);
+        buttonMultiplication = (Button) findViewById(R.id.button_multiplication);
 
+        textView = (TextView) findViewById(R.id.textView);
 
+        button0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double result;
+                try {
+                    result = Double.parseDouble(textView.toString());
+                }catch (Exception e){
+                    result = 0;
+                }
 
-
+            }
+        });
 
     }
 }
